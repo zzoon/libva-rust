8e73c3ac91ab22de1888c04e4181519ac05e3fe8
Hyunjun Ko <zzoon@igalia.com>
2017-02-13 14:42:35 +0900

    add vainfo

---
+++ vainfo/vainfo.rs
@@ -0,0 +1,41 @@
+extern crate libva_rust;
+extern crate x11;
+
+use std::env;
+use std::ptr;
+use x11::xlib::{Display, XInitThreads, XOpenDisplay, XCloseDisplay, XSync};
+
+use libva_rust::va::*;
+
+fn main() {
+    let native_display;
+
+    unsafe {
+        assert!(XInitThreads() != 0);
+        native_display = XOpenDisplay(ptr::null());
+
+        XSync(native_display, 0);
+    }
+
+    let va_disp = VADisplay::initialize(native_display as *mut VANativeDisplay).unwrap();
+    let (maj, min) = va_disp.get_va_version();
+    let name = env::args().nth(0).unwrap();
+
+    println!("{}: VA-API version: {}.{}", name, maj, min);
+    println!("{}: Driver version: {}", name, va_disp.get_vendor_string());
+    println!("{}: Supported profile and entrypoints", name);
+
+    let profiles = va_disp.get_profiles();
+
+    for profile in profiles.iter().cloned() {
+        let entries = va_disp.get_entrypoints(profile);
+        for entry in entries.iter() {
+            println!("      {:?}:\t\t{:?}", profile, entry);
+        }
+    }
+
+    va_disp.destroy();
+    unsafe {
+        XCloseDisplay(native_display as *mut Display);
+    }
+}
